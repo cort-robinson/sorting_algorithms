d9b82a0202773c84c9859a5d034897634c245b07
cort-robinson <cortrobinson99@gmail.com>
2020-10-21 09:55:22 +0000

    add arg checks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,13 +26,14 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
-	{
 		for (j = 0; j < size - i - 1; j++)
 		if (array[j] > array[j + 1])
 		{
 			swap(&array[j], &array[j + 1]);
 			print_array(array, size);
 		}
-	}
 }
