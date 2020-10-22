d7c06d42bacec2e323f26c167ac01fa66ad6682d
cort-robinson <cortrobinson99@gmail.com>
2020-10-22 00:32:55 +0000

    betty fixes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,26 +1,33 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps nodes of doubly linked list.
+ *
+ * @node2: Node to end up on right.
+ * @node1: Node to end up on left.
+ */
+
+void swap(listint_t *node2, listint_t *node1)
+{
+	listint_t *temp, *right = node2, *left = node1;
+
+	temp = left->prev;
+	left->prev = right;
+	left->next = right->next;
+	right->next = left;
+	right->prev = temp;
+	if (left->next)
+		left->next->prev = left;
+	if (right->prev)
+		right->prev->next = right;
+}
+
+/**
  * insertion_sort_list - Sorts doubly linked list of integers
  * in ascending order using insertion sort algorithm.
  *
  * @list: List to sort.
  */
-
-void swap(listint_t *node2, listint_t *node1)
-{
-    listint_t *temp, *right = node2, *left = node1;
-
-    temp = left->prev;
-    left->prev = right;
-    left->next = right->next;
-    right->next = left;
-    right->prev = temp;
-    if (left->next)
-        left->next->prev = left;
-    if (right->prev)
-        right->prev->next = right;
-}
 
 void insertion_sort_list(listint_t **list)
 {
@@ -31,21 +38,21 @@ void insertion_sort_list(listint_t **list)
 
 	nav = (*list)->next;
 
-    while (nav)
-    {
-        current = nav;
-        while (current->prev)
-        {
-            if (current->n < current->prev->n)
-            {
-                swap(current, current->prev);
-                while ((*list)->prev)
-                    *list = (*list)->prev;
-                print_list(*list);
-                continue;
-            }
-            current = current->prev;
-        }
-        nav = nav->next;
-    }
+	while (nav)
+	{
+		current = nav;
+		while (current->prev)
+		{
+			if (current->n < current->prev->n)
+			{
+				swap(current, current->prev);
+				while ((*list)->prev)
+					*list = (*list)->prev;
+				print_list(*list);
+				continue;
+			}
+			current = current->prev;
+		}
+		nav = nav->next;
+	}
 }
